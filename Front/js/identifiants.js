60a6c3be635d930edbe737ba9c695a99fcd6d7ea
trisCPE <tristan.gueneau@cpe.fr>
2019-02-01 17:20:38 +0100

    Final commit

--- Front/js/identifiants.js
+++ Front/js/identifiants.js
@@ -1,19 +1,16 @@
 
-
-
-
-
-function generateKey() {
-	var d = new Date().getTime();
+/*Génère les clés d'API*/
+const generateKey = () => {
+	let d = new Date().getTime();
 	
 	if( window.performance && typeof window.performance.now === "function" )
 	{
 		d += performance.now();
 	}
 	
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
+	let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
 	{
-		var r = (d + Math.random()*16)%16 | 0;
+		let r = (d + Math.random()*16)%16 | 0;
 		d = Math.floor(d/16);
 		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
 	});
@@ -21,24 +18,69 @@ function generateKey() {
 return uuid;
 }
 
-function printKey(){
-	document.getElementById("apikey").value = generateKey();
-	addRow(document.getElementById("apikey").value);
+/*Affiche les clés d'API*/
+const printKey = () => {
+	const nameError = document.getElementById("nameError");
+	let name = document.getElementById("basic-url").value;
+	let apiKey = document.getElementById("apikey").value;
 
+	if(name === ''){
+		document.getElementById("basic-url").focus();
+		nameError.innerHTML = "<p>Please, enter a key name</p>"
+	}
+	else {
+		document.getElementById("apikey").value = generateKey();
+		addRow(name);
+		printName(printDate());
+		printName(document.getElementById("apikey").value);
+		name = '';
+	}
 }
 
-function addRow(value){ 
+/*Affiche les noms de clé*/
+const printName = (value) => {
 
+	let tbody = document.getElementById("body");  
+	let td = document.createElement("td");
+	let tdText = document.createTextNode(value);
+
+	tbody.appendChild(td);		
+	td.appendChild(tdText);
+}
+
+/*Affiche la date*/
+const printDate = () => {
+
+	let today = new Date();
+	let dd = today.getDate();
+	let mm = today.getMonth()+1; //Janvier renvoie 0 
+	let yyyy = today.getFullYear();
+
+	if(dd<10) {
+    	dd = `0${dd}`;
+		} 
+
+    if(mm<10) {
+    	mm = `0${mm}`;
+	} 
+
+	today = `${dd} / ${mm} / ${yyyy}`;
+	return today;
+}
+
+const addRow = (value) => {
+
+	let tbody = document.getElementById('body');  
 	let tr = document.createElement('tr');
-	let tab = document.getElementById("tableau");  
-    tab.appendChild(tr);
-
-let td = document.createElement('td')
-tab.appendChild(td);
-let tdText = document.createTextNode(value);
-td.appendChild(tdText);
+    
+    tbody.appendChild(tr);
+	let td = document.createElement('td')
+	tbody.appendChild(td);
+	let tdText = document.createTextNode(value);
+	td.appendChild(tdText);
 
 }  
+
 
 
 
--- Front/js/main.js
+++ Front/js/main.js
@@ -1,18 +1,98 @@
 
-
-
-export let keys = [];
-
-function addRow(value){
+const addRow = (value) => {
   keys.push(value);
   value = '';
 }
 
-function afficher_aide(aide){
+const afficher_aide = (aide) => {
     if(aide.style.display == "none") aide.style.display ="block";
     else aide.style.display = "none";
 }
   
+/* Affichage des graphiques */
+const setupLineChart = () => {
+  let lineChartCanvas = document.getElementById("line-chart-canvas").getContext("2d");
+  return new Chart(lineChartCanvas, {
+    type: "line",
+    data: {
+      labels: ["Jan", "Fev", "Mar", "Avr", "Mai","Juin"],
+      datasets: [{
+        label: "Requêtes d'API",
+        backgroundColor: "rgba(113,0,255,0.2)",
+        borderColor: "rgba(0,0,255,1)",
+        borderWidth: 1,
+        data: [1, 3, 4, 7, 5, 6]
+        }]
+    },
+    options: {
+      responsive: false,
+      title: {
+         display: true,
+         position: 'top',
+         text: 'Trafic',
+         fontSize: 20,
+         fontColor:'#000'
+      },
+      legend: {
+          display: true,
+          position: 'bottom',
+           labels: {
+                fontColor: '#000'
+            }
+      },
+      scales: { yAxes: [{ ticks: { min: 0, max: 10, stepSize: 2} }] }
+    }
+  });
+}
 
-  
+const setupChartError = () => {
+  let lineChartError = document.getElementById("line-chart-error").getContext("2d");
+  return new Chart(lineChartError, {
+    type: "line",
+    data: {
+      labels: ["Jan", "Fev", "Mar", "Avr", "Mai", "Juin"],
+      datasets: [{
+        label: "Pourcentage d'erreurs",
+        backgroundColor: "rgba(113,0,255,0.2)",
+        borderColor: "rgba(255,0,255,1)",
+        borderWidth: 1,
+        }]
+    },
+    options: {
+      responsive: false,
+      title: {
+         display: true,
+         position: 'top',
+         text: 'Erreurs',
+         fontSize: 20,
+         fontColor:'#000'
+      },
+      legend: {
+          display: true,
+          position: 'bottom'
+      },
+      scales: {
+            yAxes: [{
+                ticks: {
+                    min: 0, max: 100, stepSize: 20,
+                    // Include a % sign after values on y-axis
+                    callback: (value, index, values) => {
+                        return  `${value} %`;
+                    }
+                }
+            }]
+        }
+    }
+  });
+}
 
+
+
+$(document).ready(function() {
+
+ var lineChart  = setupLineChart();
+ var errorChart = setupChartError();
+
+
+
+});
